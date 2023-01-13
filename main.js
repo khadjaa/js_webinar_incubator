f97ef1e258ddb1b1dd15cde3ef99a9b51559072d
khadjaa <islomkhadja2001@icloud.com>
2023-01-13 20:20:18 +0300

    main in programming

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-alert("Yo")
+// const userName = "Bobs"
+// let age = 23
+
+// if(userName === "Bob"){
+//     alert(`Hello, ${userName}`)
+// } else {
+//     alert(`I don't know you`)
+// }
+
+// for (let index = 0; index < 3; index++){
+//     alert(index)
+// }
+
+// const array = ["a", "b", "c"]
+// alert(array[0])
+
+// const user = {
+//     name: "Bob",
+//     age: 24
+// }
+// alert(user.name)
+
+// function getUser() {
+//     const name = prompt("Your friends name")
+//     const age = prompt("How old is he?")
+//     if(name) {
+//         alert(`Hello, ${name} !`)
+//     } return (
+//         {name: name, age: age}
+//     )     
+// }
+
+// const itUser = getUser()
+// console.log(itUser)
+
